39c093d31602f9e18dfbac0bc9659e01ebf51a0e
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-02-22 10:36:02 -0300

    Testes unitarios da classe Database

--- api/test/database.test.js
+++ api/test/database.test.js
@@ -14,11 +14,14 @@ describe("Database", () => {
   });
 
   test("Testa a conexão com o banco de dados", async () => {
+    // Testa a conexão com o banco de dados
     await expect(db.initialize()).resolves.toBeUndefined();
   });
 
   test("Teste de consulta com erros", async () => {
+    // Testa uma consulta com erro
     const queryWithError = "SELECT * FROM tabela_inexistente;";
+    // Verifica se a consulta com erro lança uma exceção
     await expect(db.query(queryWithError)).rejects.toThrow();
   });
 });
